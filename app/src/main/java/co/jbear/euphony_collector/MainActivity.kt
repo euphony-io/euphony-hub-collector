b61ca71e3ca4a163413d3d6dc5649e4da0d4b293
yeonns <ysh099@gmail.com>
2022-05-10 00:14:54 +0900

    init compose project

---
+++ app/src/main/java/co/jbear/euphony_collector/MainActivity.kt
@@ -0,0 +1,43 @@
+package co.jbear.euphony_collector
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import co.jbear.euphony_collector.ui.theme.EuphonyCollectorTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            EuphonyCollectorTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    EuphonyCollectorTheme {
+        Greeting("Android")
+    }
+}
