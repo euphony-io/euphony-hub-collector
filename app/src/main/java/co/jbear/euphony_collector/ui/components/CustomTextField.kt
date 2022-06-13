2273e24fa03532a5a6d6856025fbb91b5f721fc2
yeonns <ysh099@gmail.com>
2022-06-13 12:52:48 +0900

    implement speaker layout

---
+++ app/src/main/java/co/jbear/euphony_collector/ui/components/CustomTextField.kt
@@ -0,0 +1,43 @@
+package co.jbear.euphony_collector.ui.components
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.SpeakerPhone
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import co.jbear.euphony_collector.ui.theme.LightSkyBlue
+import co.jbear.euphony_collector.ui.theme.Typography
+
+@Composable
+fun CustomTextField(
+    text: String,
+    onTextChanged: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    TextField(
+        modifier = modifier,
+        shape = RoundedCornerShape(14.dp),
+        trailingIcon = { Icon(Icons.Outlined.SpeakerPhone, "", tint = Color.Black) },
+        colors = TextFieldDefaults.textFieldColors(
+            backgroundColor = LightSkyBlue,
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent,
+            cursorColor = Color.Black
+        ),
+        placeholder = {
+            Text(
+                text = "Input Text",
+                style = Typography.body1
+            )
+        },
+        value = text,
+        onValueChange = { onTextChanged(it) },
+        singleLine = true
+    )
+}
--- app/src/main/java/co/jbear/euphony_collector/ui/view/CollectorView.kt
+++ app/src/main/java/co/jbear/euphony_collector/ui/view/CollectorView.kt
@@ -1,22 +1,69 @@
 package co.jbear.euphony_collector.ui.view
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import co.jbear.euphony_collector.ui.components.CustomTextField
+import co.jbear.euphony_collector.ui.theme.EuphonyCollectorTheme
 import com.ramcosta.composedestinations.annotation.Destination
 
 @Composable
 @Destination(route = "home/collector_view", start = true)
 fun CollectorView() {
+
+    var textToSend by rememberSaveable { mutableStateOf("") }
+
     Column(
         modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
+        verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text("collector")
+        Header()
+        CustomTextField(
+            text = textToSend,
+            onTextChanged = { textToSend = it },
+            modifier = Modifier
+                .padding(top = 40.dp)
+                .height(48.dp)
+        )
+    }
+}
+
+@Composable
+private fun Header() {
+    Text(
+        modifier = Modifier.padding(top = 80.dp),
+        text = "Euphony Data Collector",
+        style = MaterialTheme.typography.h3
+    )
+
+    Text(
+        modifier = Modifier.padding(
+            top = 16.dp,
+            start = 30.dp,
+            end = 30.dp,
+        ),
+        text = "Collector provides both speaker and listener mode, \n" +
+                "so you can send data using euphony and get the result.",
+        textAlign = TextAlign.Center,
+        style = MaterialTheme.typography.body1
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CollectorPreview() {
+    EuphonyCollectorTheme {
+        CollectorView()
     }
 }
