a1da4efd56d400e2bfc8178ddf30d697f1f96506
yeonns <ysh099@gmail.com>
2022-07-28 23:35:43 +0900

    fix: update test code

--- app/src/main/java/co/jbear/euphony_hub/data/repository/PreferenceRepository.kt
+++ app/src/main/java/co/jbear/euphony_hub/data/repository/PreferenceRepository.kt
@@ -4,12 +4,17 @@ import android.content.Context
 import androidx.datastore.dataStore
 import co.jbear.euphony_hub.data.preference.AppPreferencesSerializer
 import co.jbear.euphony_hub.domain.entity.AppPreference
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.android.scopes.ActivityRetainedScoped
+import javax.inject.Inject
 
-class PreferenceRepository(context: Context) {
-    private val Context.protoDataStore by dataStore(
-        fileName = "euphony-setting.json",
-        serializer = AppPreferencesSerializer,
-    )
+private val Context.protoDataStore by dataStore(
+    fileName = "euphony-setting.json",
+    serializer = AppPreferencesSerializer,
+)
+
+@ActivityRetainedScoped
+class PreferenceRepository @Inject constructor(@ApplicationContext context: Context) {
 
     private val appDataStore = context.protoDataStore
     val data = appDataStore.data
