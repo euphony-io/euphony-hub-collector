12c58628c9e245d50404b368a22751f2ced4ac48
yeonjiyeon <dalnim95@naver.com>
2022-07-30 22:31:32 +0900

    chore: euphony version update

---
+++ app/src/main/java/co/jbear/euphony_hub_collector/ui/view/CollectorView.kt
@@ -0,0 +1,146 @@
+package co.jbear.euphony_hub_collector.ui.view
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.PauseCircleFilled
+import androidx.compose.material.icons.outlined.SpeakerPhone
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import co.jbear.euphony_hub_collector.data.repository.PreferenceRepository
+import co.jbear.euphony_hub_collector.presentation.collector.CollectorViewModel
+import co.jbear.euphony_hub_collector.ui.components.CustomTextField
+import co.jbear.euphony_hub_collector.ui.theme.EuphonyHubCollectorTheme
+import co.jbear.euphony_hub_collector.ui.theme.SkyBlue
+import com.ramcosta.composedestinations.annotation.Destination
+
+@Composable
+@Destination(route = "home/collector_view", start = true)
+fun CollectorView(
+    viewModel: CollectorViewModel = hiltViewModel()
+) {
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Header()
+        Speaker(viewModel)
+    }
+    Listener(viewModel)
+}
+
+@Composable
+private fun Header() {
+    Text(
+        modifier = Modifier.padding(top = 80.dp),
+        text = "Euphony Hub Collector",
+        style = MaterialTheme.typography.h3
+    )
+
+    Text(
+        modifier = Modifier.padding(
+            top = 16.dp,
+            start = 30.dp,
+            end = 30.dp,
+        ),
+        text = "Euphony Hub Collector provides both speaker and listener mode, \n" +
+               "so you can send data using euphony and get the result. \n" +
+               "To send clearly, please set device volume to max.",
+        textAlign = TextAlign.Center,
+        style = MaterialTheme.typography.body1
+    )
+}
+
+@Composable
+private fun Speaker(viewModel: CollectorViewModel) {
+    val isListening by viewModel.isListening.observeAsState(false)
+    var textToSend by rememberSaveable { mutableStateOf("") }
+
+    CustomTextField(
+        text = textToSend,
+        onTextChanged = { textToSend = it },
+        trailingIcon = {
+            if (textToSend.isNotEmpty()) {
+                IconButton(onClick = {
+                    viewModel.listen()
+                    viewModel.speak(textToSend)
+                }) {
+                    if (isListening) {
+                        Icon(Icons.Outlined.PauseCircleFilled, "", tint = Color.Black, modifier = Modifier.testTag("pauseButton"))
+                    } else {
+                        Icon(Icons.Outlined.SpeakerPhone, "", tint = Color.Black, modifier = Modifier.testTag("speakerButton"))
+                    }
+                }
+            }
+        },
+        modifier = Modifier
+            .testTag("textToSend")
+            .padding(top = 40.dp)
+            .width(300.dp)
+            .height(48.dp)
+    )
+}
+
+@Composable
+private fun Listener(viewModel: CollectorViewModel) {
+    val isProcessing by viewModel.isProcessing.observeAsState(false)
+    val listenResult by viewModel.listenResult.observeAsState("")
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        if (isProcessing) {
+            CircularProgressIndicator(
+                modifier = Modifier
+                    .size(80.dp),
+                color = SkyBlue,
+                strokeWidth = 10.dp
+            )
+        } else if (listenResult.isNotEmpty()) {
+            val color = if (viewModel.getResult().equals("SUCCESS")) {
+                SkyBlue
+            } else {
+                Color.Red
+            }
+
+            Text(
+                text = viewModel.getResult(),
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.h3,
+                color = color
+            )
+            Text(
+                text = "\"$listenResult\"",
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.body2
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CollectorPreview() {
+    EuphonyHubCollectorTheme {
+        CollectorView(
+            viewModel = CollectorViewModel(PreferenceRepository(LocalContext.current))
+        )
+    }
+}
--- app/src/main/java/co/jbear/euphony_hub_controller/domain/entity/BaseType.kt
+++
@@ -1,6 +0,0 @@
-package co.jbear.euphony_hub_controller.domain.entity
-
-enum class BaseType(val value: Int) {
-    BASE2(0),
-    BASE16(1),
-}
